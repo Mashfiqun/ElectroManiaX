01b850c07e9f31eec47ee578614e092be467a43d
Mashfiqun Nabi <mashnabi99@gmail.com>
2024-09-08 18:41:13 +0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -149,5 +149,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "electromaniax2024@gmail.com"
-EMAIL_HOST_PASSWORD = ""
+EMAIL_HOST_PASSWORD = "vczu cdpu amwh omut"
 EMAIL_PORT = 587
